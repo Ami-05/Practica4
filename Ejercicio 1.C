33470e3cb4c309f14de4c2d070ab01a679966531
Ami-05 <cruzapodacaamielmerari@gmail.com>
2025-03-06 14:04:27 -0600

    Create Ejercicio 1.C

---
+++ Ejercicio 1.C
@@ -0,0 +1,16 @@
+//Cruz Apodaca Amiel Merari
+//Ejercicio 1
+#include <stdio.h>
+
+int main()
+{
+    int a=3;
+    int b=2;
+    if(a>b)
+      
+    {
+        printf("a es mayor\n");
+    }
+    return 0;
+    
+}
